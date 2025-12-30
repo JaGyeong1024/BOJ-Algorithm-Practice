6e8c2e9cc7dff106c7dd6de08167933c7cc23f65
JaGyeong Gu <92356313+JaGyeong1024@users.noreply.github.com>
2025-12-31 02:20:09 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<stack>
 #include<string>
-#include<sstream>
 using namespace std;
 
 int main() {
@@ -10,34 +9,39 @@ int main() {
 	cin.tie(nullptr);
 
 	stack<int> s;
-	string count;
+	int N;
 
-	getline(cin, count, '\n');
-	int N = stoi(count);
+	cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		string oper, inst;
+		string inst;
 		int x = 0;
-
-		getline(cin, oper, '\n');
-		stringstream ss(oper);
-		ss >> inst;
+		
+		cin >> inst;
 
 		if (inst == "push") {
-			ss >> x;
+			cin >> x;
 			s.push(x);
 		}
-		else if (inst == "pop")
+		else if (inst == "pop") {
 			if (s.empty() != true) {
 				cout << s.top() << "\n";
 				s.pop();
 			}
-			else cout << "-1" << "\n";
+			else {
+				cout << -1 << "\n";
+			}
+		}
 		else if (inst == "size") cout << s.size() << "\n";
 		else if (inst == "empty") cout << (s.empty() ? 1 : 0) << "\n";
-		else if (inst == "top")
-			if (s.empty() != true) cout << s.top() << "\n";
-			else cout << "-1" << "\n";
+		else if (inst == "top") {
+			if (s.empty() != true) {
+				cout << s.top() << "\n";
+			}
+			else {
+				cout << -1 << "\n";
+			}
+		}
 	}
 
 	return 0;
